7f9d06abe31959e1436a888decb3dcb899ab08f1
victorVaflo <victorvaflo@gmail.com>
2021-01-29 14:35:00 -0500

    👽 gulp watch gulp-rev and gulp-rewritte

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,65 @@
-const { series, parallel, dest, watch } = require('gulp');
+const { series, dest, watch, src } = require('gulp');
 const gulp = require('gulp');
 const gulpEsbuild = require('gulp-esbuild');
 const pug = require('gulp-pug');
 const minHtml = require('gulp-htmlmin');
-const uglify = require('gulp-uglify-es').default;
+const rev = require('gulp-rev');
 const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
 const svgmin = require('gulp-svgmin');
 const sass = require('gulp-sass');
-const hash = require("gulp-hash-filename");
+const nodemon = require('gulp-nodemon');
+const revRewrite = require('gulp-rev-rewrite');
 
 sass.compiler = require('node-sass');
 
 gulp.task('build', function (done) {
-    gulp.src(['src/scripts/*.ts', 'src/scripts/**/*.ts'], { base: 'src/scripts/' })
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulpEsbuild({}))
-        .pipe(hash({ "format": "{name}.{hash}.{ext}" }))
-        .pipe(gulp.dest('dist'));
-
-    gulp.src(['src/*.pug', 'src/pages/*.pug', 'src/pages/**/*pug'], { base: 'src/' })
+    gulp.src(['src/**/*.pug'], { base: 'src/' })
         .pipe(pug({}))
         .pipe(minHtml({ collapseWhitespace: true }))
         .pipe(gulp.dest('dist'));
 
-    gulp.src(['src/assets/images/*.png', 'src/assets/images/favicon/*.png'], { base: 'src/assets/images/' })
+    gulp.src(['src/**/*.ts'], { base: 'src/' })
+        .pipe(sourcemaps.init())
+        .pipe(gulpEsbuild({}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(rev())
+        .pipe(src('dist/**/*.html'))
+        .pipe(revRewrite())
+        .pipe(gulp.dest('dist/assets/scripts'));
+
+    gulp.src(['src/**/*.png'], { base: 'src/' })
         .pipe(imagemin())
-        .pipe(hash({ "format": "{name}.{hash}.{ext}" }))
+        .pipe(rev())
+        .pipe(src('dist/**/*.html'))
+        .pipe(revRewrite())
         .pipe(gulp.dest('dist'));
 
-    gulp.src(['src/assets/illustrations/*.svg', 'src/assets/images/*.svg', 'src/assets/images/**/*.svg'], { base: 'src/assets/' })
+    gulp.src(['src/**/*.svg'], { base: 'src/' })
         .pipe(sourcemaps.init())
         .pipe(svgmin())
         .pipe(sourcemaps.write('.'))
-        .pipe(hash({ "format": "{name}.{hash}.{ext}" }))
+        .pipe(rev())
+        .pipe(src('dist/**/*.html'))
+        .pipe(revRewrite())
         .pipe(gulp.dest('dist'));
 
-    gulp.src(['src/styles/*.scss', 'src/styles/**/*.scss', 'src/styles/**/**/*.scss'], { base: 'src/styles/' })
+    gulp.src(['src/**/*.scss'], { base: 'src/' })
         .pipe(sass().on('error', sass.logError))
-        .pipe(hash({ "format": "{name}.{hash}.{ext}" }))
+        .pipe(rev())
+        .pipe(src('dist/**/*.html'))
+        .pipe(revRewrite())
         .pipe(gulp.dest('dist'));
+
     done();
 });
+gulp.task('watch', gulp.series('build', function (done) {
+    var stream = nodemon({
+        script: 'server.js'
+        , watch: 'build'
+        , tasks: ['build']
+        , done: done
+    })
+
+    return stream
+}))
